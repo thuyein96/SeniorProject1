ca259b7be3083e50daf160cfe004f2a819e51b9a
Thu Yein <thuyein1996alpha@gmail.com>
2024-11-24 11:40:11 +0700

    Timer fixed

--- SnowFlake/Hubs/TimerHub.cs
+++ SnowFlake/Hubs/TimerHub.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Threading;
+using Microsoft.AspNetCore.SignalR;
 
 namespace SnowFlake.Hubs;
 
 public class TimerHub : Hub
 {
-    public async Task SendTimerUpdate(string connectionId, int secondsRemaining)
+    public async Task SendTimerUpdate(int secondsRemaining)
     {
-        for (int i = 0; i < secondsRemaining; i++)
+        while (secondsRemaining > 0) // Loop until the countdown reaches 0
         {
-            var currentTime = DateTime.Now;
+            var timer = TimeOnly.FromTimeSpan(TimeSpan.FromSeconds(secondsRemaining));
 
-            Thread.Sleep(1000);
-            await Clients.Client(connectionId).SendAsync("ReceiveTimerUpdate", currentTime);
+            // Send the timer update to all clients
+            await Clients.All.SendAsync("ReceiveTimerUpdate", timer.ToString("mm:ss"));
+
+            // Decrement the remaining seconds
+            secondsRemaining--;
+
+            // Wait for 1 second
+            await Task.Delay(1000);
         }
+
+        // Send a final update to indicate the timer has reached 0
+        await Clients.All.SendAsync("ReceiveTimerUpdate", "00:00");
     }
 }
