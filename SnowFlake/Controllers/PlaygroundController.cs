54200c9983b1a47a51ec4a717d10002f678c1daa
Thu Yein <thuyein1996alpha@gmail.com>
2025-02-18 13:26:12 +0700

    playground namespace align

--- SnowFlake/Controllers/PlaygroundController.cs
+++ SnowFlake/Controllers/PlaygroundController.cs
@@ -5,81 +5,80 @@ using SnowFlake.Dtos.APIs.Playground.GetPlayground;
 using SnowFlake.Managers;
 using SnowFlake.Services;
 
-namespace SnowFlake.Controllers
+namespace SnowFlake.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlaygroundController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PlaygroundController : ControllerBase
+    private readonly IPlaygroundService _playgroundService;
+    private readonly IPlaygroundManager _playgroundManager;
+
+    public PlaygroundController(IPlaygroundService playgroundService,
+        IPlaygroundManager playgroundManager)
     {
-        private readonly IPlaygroundService _playgroundService;
-        private readonly IPlaygroundManager _playgroundManager;
+        _playgroundService = playgroundService;
+        _playgroundManager = playgroundManager;
+    }
 
-        public PlaygroundController(IPlaygroundService playgroundService,
-                                    IPlaygroundManager playgroundManager)
+    [HttpPost]
+    public async Task<IActionResult> Entry(ConfigurePlaygroundRequest request)
+    {
+        try
         {
-            _playgroundService = playgroundService;
-            _playgroundManager = playgroundManager;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Entry(ConfigurePlaygroundRequest request)
-        {
-            try
+            if (string.IsNullOrWhiteSpace(request.HostRoomCode) ||
+                string.IsNullOrWhiteSpace(request.PlayerRoomCode))
             {
-                if (string.IsNullOrWhiteSpace(request.HostRoomCode) ||
-                    string.IsNullOrWhiteSpace(request.PlayerRoomCode))
-                {
-                    return BadRequest("Require player or host room code.");
-                }
+                return BadRequest("Require player or host room code.");
+            }
 
-                if (request.Rounds.Count <= 0) return BadRequest("Need to have at least 1 round.");
+            if (request.Rounds.Count <= 0) return BadRequest("Need to have at least 1 round.");
 
-                var playgroundResponse = await _playgroundManager.SetupPlayground(request);
+            var playgroundResponse = await _playgroundManager.SetupPlayground(request);
 
-                if (playgroundResponse == null)
+            if (playgroundResponse == null)
+            {
+                return NotFound(new CreatePlaygroundResponse
                 {
-                    return NotFound(new CreatePlaygroundResponse
-                    {
-                        Success = false,
-                        Message = null
-                    });
-                }
-                return Ok(new CreatePlaygroundResponse
-                {
-                    Success = true,
-                    Message = playgroundResponse
+                    Success = false,
+                    Message = null
                 });
             }
-            catch (Exception e)
+            return Ok(new CreatePlaygroundResponse
             {
-                return StatusCode(500, e.Message);
-            }
+                Success = true,
+                Message = playgroundResponse
+            });
         }
-
-        [HttpGet("HostRoomCode")]
-        public async Task<IActionResult> GetByRoomCode(string hostRoomCode)
+        catch (Exception e)
         {
-            try
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpGet("HostRoomCode")]
+    public async Task<IActionResult> GetByRoomCode(string hostRoomCode)
+    {
+        try
+        {
+            var playgroundResponse = await _playgroundService.GetPlayground(hostRoomCode);
+            if (playgroundResponse == null)
             {
-                var playgroundResponse = await _playgroundService.GetPlayground(hostRoomCode);
-                if (playgroundResponse == null)
+                return NotFound(new GetPlaygroundByRoomCodeResponse
                 {
-                    return NotFound(new GetPlaygroundByRoomCodeResponse
-                    {
-                        Success = false,
-                        Message = null
-                    });
-                }
-                return Ok(new GetPlaygroundByRoomCodeResponse
-                {
-                    Success = true,
-                    Message = playgroundResponse
+                    Success = false,
+                    Message = null
                 });
             }
-            catch (Exception e)
+            return Ok(new GetPlaygroundByRoomCodeResponse
             {
-                return StatusCode(500, e.Message);
-            }
+                Success = true,
+                Message = playgroundResponse
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
         }
     }
 }
